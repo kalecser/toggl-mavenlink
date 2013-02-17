f7a35ce7ea90e986e74adc1be143e7ae449d572d
kalecser <kalecser@gmail.com>
2013-02-16 23:34:29 -0200

    Toggl Mavenlink Comparison

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app
 .post('/toggl', function (req, resp){
 	toggl(req.body, resp);
 })
+.post('/mavenlink', function(req, resp){
+	mavenlink(req.body, resp);
+})
 .get("/*", function (req, resp){
 	file.serve(req, resp);
 }).listen(8000);
@@ -20,34 +23,60 @@ app
 
 var toggl = function(req, resp){
 
-var request = require('request'),
-    password = "api_token",
-    username = req.togglToken,
-    url = "https://www.toggl.com/api/v6/time_entries.json",
-    auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
-
-request(
-    {
-        url : url,
-        headers : {
-            "Authorization" : auth
-        }
-    },
-    function (error, response, body) {
-
-
-	try {
+	var url = "https://www.toggl.com/api/v6/time_entries.json";
+	var onResponse = function(body, resp) {
 		var result = _u(JSON.parse(body).data).map(function(entry) {
-			return [entry.description,
-				entry.start.substring(0,10),
+			return [
+				entry.start.substring(0,10).replace(/-/g, "/"),
 				(entry.duration / 60) /60 ]	
 		});
 		resp.json(result);
-	}catch (ex) {
-		resp.json({error: 'Error accessing toggl, your api token : ' + req.togglToken + ' may not be valid'});
-	}
+		};
 
-    }
-);
-	
+	requestWithBasicAuth(req.togglToken, "api_token", url, onResponse, resp);
+
 };
+
+var mavenlink = function(req, resp){
+ 	var url = "https://www.mavenlink.com/api/v0/workspaces.json";
+	var mavenlinkToken = req.mavenlinkToken;
+	var mavenlinkUser = req.mavenlinkUser;
+        var onResponse = function(body, resp) {
+		var workspace = JSON.parse(body)[0].id;
+		requestWithBasicAuth(mavenlinkUser, mavenlinkToken, "https://www.mavenlink.com/api/v0/workspaces/" + workspace + "/time_entries.json", 
+		function(tbody, resp) {
+			var result = JSON.parse(tbody).map(function(timeEntry){
+				return [timeEntry.date_performed, 
+					timeEntry.time_in_minutes/60];
+			});
+			resp.json(result);
+		}, resp);
+        };
+
+        requestWithBasicAuth(mavenlinkUser, mavenlinkToken,  url, onResponse, resp);
+};
+
+var requestWithBasicAuth = function(username, password, url, onResponse, resp) {
+		if (resp == null) throw "resp is required";
+		var request = require('request'),
+		auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+
+		request(
+		    {
+			url : url,
+			headers : {
+			    "Authorization" : auth
+			}
+		    },
+		    function (error, response, body) {
+
+
+			try {
+				onResponse(body, resp);
+			}catch (ex) {
+				resp.json({error: 'Error accessing remote service, check your user/pass : ' + username + ':' + password + ' may not be valid<br>' + ex });
+			}
+
+		    }
+		);
+	};
